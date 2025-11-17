d5bd8947bb183fbdf4826cf07905f36dbe32de32
shivampawar777 <pawarshivam826@gmail.com>
2025-11-17 22:08:17 +0530

    Update 8

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -4,11 +4,11 @@ def call() {
                         usernameVariable: "DOCKER_USER",
                         passwordVariable: "DOCKER_TOKEN")
     ]) {
-            sh """
-                cd ${env.DockerFile_path}
-                docker build -t ${DOCKER_USER}/streamlit_application:${env.IMAGE_TAG} .
-                docker login -u ${DOCKER_USER} -p ${DOCKER_TOKEN}
-                docker push ${DOCKER_USER}/streamlit_application:${env.IMAGE_TAG}
-            """
+        sh """
+            cd ${env.DockerFile_path}
+            docker build -t ${DOCKER_USER}/streamlit_application:${env.IMAGE_TAG} .
+            docker login -u ${DOCKER_USER} -p ${DOCKER_TOKEN}
+            docker push ${DOCKER_USER}/streamlit_application:${env.IMAGE_TAG}
+        """
     }
 }
--- vars/updateDeploymentManifest.groovy
+++ vars/updateDeploymentManifest.groovy
@@ -6,17 +6,17 @@ def call(){
                         usernameVariable: "DOCKER_USER",
                         passwordVariable: "DOCKER_TOKEN")
     ]) {
-            sh """
-                git config user.email "${GITHUB_EMAIL}"
-                git config user.name "${env.GITHUB_USER}"
+        sh """
+            git config user.email "${GITHUB_EMAIL}"
+            git config user.name "${env.GITHUB_USER}"
 
-                # update the deployment file: replace image tag
-                sed -i "s|${DOCKER_USER}/streamlit-app:.*|${DOCKER_USER}/streamlit-app:${env.IMAGE_TAG}|g" \
-                ${env.deployment_file_path}
-                        
-                git add ${env.deployment_file_path}
-                git commit -m "Updated deployment image to version ${env.IMAGE_TAG}"
-                git push https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${env.GITHUB_REPO} HEAD:main
-            """        
+            # update the deployment file: replace image tag
+            sed -i "s|${DOCKER_USER}/streamlit-app:.*|${DOCKER_USER}/streamlit-app:${env.IMAGE_TAG}|g" \
+            ${env.deployment_file_path}
+                    
+            git add ${env.deployment_file_path}
+            git commit -m "Updated deployment image to version ${env.IMAGE_TAG}"
+            git push https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${env.GITHUB_REPO} HEAD:main
+        """        
     }
 }
